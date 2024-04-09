4eb4c0fe2d546b244cb6301b80c2473fe8ba7f5f
Sonny Mayta <mayta.sonny@gmail.com>
2024-04-09 11:21:17 -0400

    Compilación

---
+++ Compilar/saludo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola, mundo!" << endl;
+    return 0;
+}
+
+/*
+    COMPILAR
+    Es pasar el código fuente a código maquina.
+    * Lenguajes de programación de alto nivel son similares al lenguaje humano.
+    * Lenguajes de bajo nivel son más aproximados al código maquina.
+    Proceso:
+    1 - Preprocesado: Ver todas las directivas para importar el código que hay en esas librerias.
+    2 - Compilación: Generar las instrucciones de ensamblado según la máquina donde estas compilando el programa.
+    3 - Ensamblado: Las instruciones de ensamblado se combierten a código máquina.
+    4 - Enlazado: Se fusionan todos los archivos que se hayan generado en el proceso en un archivo ejecutable.
+*/
