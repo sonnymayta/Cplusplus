dcd12ed2e8d483e450a4a79331c0ef77c2f6544b
Sonny Mayta <mayta.sonny@gmail.com>
2024-04-16 09:07:33 -0400

    Conceptos basicos arrays

---
+++ Arrays/program.cpp
@@ -0,0 +1,18 @@
+/*
+    Arrays
+    - Es una estructura de datos que contiene una coleción de valores del mismo tipo.
+    - Sirven para almacenar valores que normalmente tiene alguna relación entre sí.
+    - Su sintaxis en C++ es: int mi_matriz[n];
+
+    Declaraciones:
+    int mi_matriz[5] {15, 24, 4, -5}; [15][24][4][-5]
+    
+    int mi_matriz[5] {15, 24} [15][24][0][0]
+
+    const double salario {20}; 
+    double mi_matriz[salario] {2700,35} [2700,35][2700,35][2700,35][2700,35]...
+
+    int mi_matriz[] {15, 14, 23, 4}; [15][14][23][4]
+
+    Se enumera las posiciones de los arrays desde 0 a n-1 donde n es el tamaño del array.
+*/
