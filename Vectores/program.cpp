7e7a2888a7c785436cebe0e9f8a49fdf5c5bf30d
Sonny Mayta <mayta.sonny@gmail.com>
2024-04-24 12:10:24 -0400

    Vectores

---
+++ Vectores/program.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // Declarando e inicializando vectores
+
+    // vector <int> records(5);
+    vector <int> records {1, 2, 3, 4, 5};
+
+    // vector <char> letras (10);
+    vector <char> letras {'a', 'b', 'c'};
+
+    vector <double> salarioBase (350, 2200.50); // Vector asignandole un valor por defecto
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << records[i] << endl;
+    }
+
+    for (int  i = 0; i < 3; i++)
+    {
+        cout << letras[i] << endl;
+    }
+    
+    int i = 0;
+    while (i < salarioBase.size())
+    {
+        cout << (i + 1) << " - " << salarioBase[i] << endl;
+        i++;
+    }   
+    
+}
+
+/*
+    VECTOR
+
+    - Vector es una clase de la librería de C++. Esta clase crea estructuras.
+    contenedoras para almacenar datos secuenciales.
+    - Los vectores pueden crecer o decrecer en tamaño de forma dinámica.
+    - Tienen un sintaxis muy similar a los Arrays.
+    - Proporciona múltiples métodos para chequear limites, tamaño comprobar si existe un elemento, etc.
+    - Almacenan los valores en posiciones contiguas de la memoria y también en posiciones separadas
+    accesibles a través de punteros.
+
+    Características
+    - Tamaño dinámico.
+    - Los elementos almacenados en su interior son del mismo tipo.
+    - Se accede a los elementos almacenados con su posición o índice.
+    - El primer elemento tiene índice 0.
+    - El último elemento tiene índice n+1.
+*/
