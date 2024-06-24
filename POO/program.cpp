943bfb73e2cf61ea6c89f40d63bf002e6ffef2b7
Sonny Mayta <mayta.sonny@gmail.com>
2024-06-24 09:09:17 -0400

    POO introducción

---
+++ POO/program.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
+
+/*
+    POO (Programación Orientada a Objetos)
+
+    - Consiste en trasladar la naturaleza de los objectos de la vida real al código de programación.
+    - Las caracteristicas de una objeto de la vida real tienen estados, un comportamiento, y
+    unas propiedades.
+    
+    Ventajas:
+    - Modularización
+    - Muy reutilizable (herencia)
+    - Si existe en alguna linea del código, el programa continuará con su funcionamiento.
+    Tratamiento de excepciones
+    - Encapsulamiento
+
+    Terminos en POO
+    - Clase
+    - Objeto
+    - Ejemplar de clase, instancia de clase, ejemplarizar una clase, instanciar una clase
+    - Modularización
+    - Encapsulamiento / encapsulación
+    - Herencia
+    - Polimorfismo
+*/
